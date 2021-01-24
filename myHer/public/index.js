5d75bbca046445b6d6503ae40c6059e60d461f86
Semat <s.igbaria@gmail.com>
2021-01-24 18:57:38 +0200

    22

---
+++ myHer/public/index.js
@@ -0,0 +1,44 @@
+  
+document.querySelector('h1').style.background = 'yellow';
+const root = document.querySelector('#root')
+
+
+function getUsers(e){
+    console.log('get users')
+
+    fetch('/get-users')
+    .then(r=>r.json())
+    .then(data=>{
+        console.log('then')
+        renderUsers(data)
+        
+    })
+    console.log('after fetch')
+}
+//////////////////////////////////////
+function addNewUser(e){
+    post('/get-users',(req,result)=>{})
+
+    
+    
+    console.log('get new users')
+
+    fetch('/get-users')
+    .then(r=>r.json())
+    .then(data=>{
+        console.log('then')
+        renderUsers(data)
+        
+    })
+    console.log('after fetch')
+}
+
+////////////////////////////////////
+function renderUsers(users){
+    let html = '';
+    users.forEach(user=>{
+        html += `<p>Name: ${user.name}</p>`
+    }) 
+
+    root.innerHTML = html
+}
--- myHer/server.js
+++ myHer/server.js
@@ -1,13 +1,47 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
-// var path= require('path');
-const port=process.env.PORT || 3000;
+
 app.use(express.static('public'));
 
+const users = [
+  {name:'Kilani', id:'222222222'},
+  {name:'Yara', id:'3333333'},
+  {name:'Ihab', id:'444444'}
+]
 
-app.listen(port,function(){
-console.log('listening',port)
-}
-)
+//route
+app.get('/get-users',(req, res)=>{
+  res.send(users)
+})
+
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log('listening', port)
+})
+
+
+
+
+
+
+
+
+
+// const express = require('express')
+// const app = express();
+
+// // var path= require('path');
+// const port=process.env.PORT || 3000;
+// app.use(express.static('public'));
+
+
+// app.listen(port,function(){
+// console.log('listening',port)
+// }
+// )
 
